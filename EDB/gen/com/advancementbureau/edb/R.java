1ef17a682df823e4f0392781748f08653b9500a7
Tesla <Tesla@Wardenclyffe>
2012-06-08 10:09:44 -0700

    skajdf

--- EDB/gen/com/advancementbureau/edb/R.java
+++ EDB/gen/com/advancementbureau/edb/R.java
@@ -14,19 +14,8 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black=0x7f06000b;
-        public static final int darker_grey=0x7f06000c;
-        public static final int five_color=0x7f060000;
-        public static final int five_color_fade=0x7f060005;
-        public static final int four_color=0x7f060001;
-        public static final int four_color_fade=0x7f060006;
-        public static final int grey=0x7f06000a;
-        public static final int one_color=0x7f060004;
-        public static final int one_color_fade=0x7f060009;
-        public static final int three_color=0x7f060002;
-        public static final int three_color_fade=0x7f060007;
-        public static final int two_color=0x7f060003;
-        public static final int two_color_fade=0x7f060008;
+        public static final int black=0x7f060001;
+        public static final int grey=0x7f060000;
     }
     public static final class dimen {
         public static final int big_button=0x7f070002;
