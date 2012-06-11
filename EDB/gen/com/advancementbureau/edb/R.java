8d7d45ba2681f0378b3090a0579de95195a75f6a
Adriano Bianchini <animationbureau@gmail.com>
2012-06-10 17:09:45 -0700

    Done with Write Tab, almost done with Read Tab

--- EDB/gen/com/advancementbureau/edb/R.java
+++ EDB/gen/com/advancementbureau/edb/R.java
@@ -29,15 +29,18 @@ public final class R {
     public static final class drawable {
         public static final int add_dark=0x7f020000;
         public static final int add_light=0x7f020001;
-        public static final int alert=0x7f020002;
-        public static final int delete_dark=0x7f020003;
-        public static final int delete_light=0x7f020004;
-        public static final int edb_logo=0x7f020005;
-        public static final int help=0x7f020006;
-        public static final int ic_launcher=0x7f020007;
-        public static final int launcher_icon=0x7f020008;
-        public static final int new_file_dialog_screenshot=0x7f020009;
-        public static final int time=0x7f02000a;
+        public static final int add_line_one_screenshot=0x7f020002;
+        public static final int add_line_two_screenshot=0x7f020003;
+        public static final int alert=0x7f020004;
+        public static final int delete_dark=0x7f020005;
+        public static final int delete_light=0x7f020006;
+        public static final int edb_logo=0x7f020007;
+        public static final int help=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int launcher_icon=0x7f02000a;
+        public static final int new_file_dialog_screenshot=0x7f02000b;
+        public static final int read_file_screenshot=0x7f02000c;
+        public static final int time=0x7f02000d;
     }
     public static final class id {
         public static final int AddData_Layout=0x7f0a000a;
@@ -83,10 +86,14 @@ public final class R {
     }
     public static final class string {
         public static final int about=0x7f08000a;
+        public static final int add_line=0x7f080012;
+        public static final int add_line_one=0x7f080013;
+        public static final int add_line_two=0x7f080014;
         public static final int app_name=0x7f080001;
         public static final int changelog=0x7f080002;
         public static final int database=0x7f080004;
         public static final int delete=0x7f08000f;
+        public static final int delete_file=0x7f080019;
         public static final int encrypted=0x7f080003;
         public static final int enter=0x7f08000e;
         public static final int first_dialog_text=0x7f080005;
@@ -95,9 +102,13 @@ public final class R {
         public static final int new_file=0x7f08000b;
         public static final int new_file_one=0x7f080010;
         public static final int new_file_prompt=0x7f08000c;
+        public static final int new_file_title=0x7f080015;
         public static final int new_file_two=0x7f080011;
         public static final int read=0x7f080009;
         public static final int read_cap=0x7f080007;
+        public static final int read_file=0x7f080016;
+        public static final int read_file_one=0x7f080017;
+        public static final int read_file_two=0x7f080018;
         public static final int read_write=0x7f08000d;
         public static final int write=0x7f080008;
     }
